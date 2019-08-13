c7caf082df0cfdaf6035cbf72bcb302d56bcee9c
Garmahis666 <garmahis@garmahis.ru>
2019-08-13 13:38:47 +0300

    переименована переменная

--- src/main.js
+++ src/main.js
@@ -469,7 +469,7 @@ const getCardEditMarkup = () => `<article class="card card--edit card--yellow ca
 const mainElement = document.querySelector(`.main`);
 const headerElement = document.querySelector(`.main__control`);
 
-const cardsDB = [{text: `Example default task with custom color.`, color: ``, repeat: false, deadLine: null, tags: [], edit: true},
+const testCards = [{text: `Example default task with custom color.`, color: ``, repeat: false, deadLine: null, tags: [], edit: true},
   {text: `Next example task with deadline`, color: `red`, repeat: false, deadLine: new Date(2019, 7, 10, 12, 0, 0), tags: [], edit: false},
   {text: `Next example task with tags`, color: `pink`, repeat: false, deadLine: new Date(2019, 11, 22, 18, 0, 0), tags: [`#День Рождения`, `#Бухаем!`]}];
 
@@ -485,7 +485,7 @@ const prepareSite = () => {
   render(mainElement, getContentMarkup());
   const boardElement = document.querySelector(`.board`);
   const boardTasksElement = boardElement.querySelector(`.board__tasks`);
-  cardsDB.forEach(({text, color, repeat, deadLine, tags, edit}) => {
+  testCards.forEach(({text, color, repeat, deadLine, tags, edit}) => {
     let newCard = edit ? getCardEditMarkup() : getCardMarkup(text, color, repeat, deadLine, tags);
     render(boardTasksElement, newCard);
   });
